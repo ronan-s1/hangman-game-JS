3834dfa18bd86376e9f967d60eccd6385feab6d1
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2021-12-05 05:08:14 +0000

    removed alphabet array

--- hangman/assets/js/script.js
+++ hangman/assets/js/script.js
@@ -6,8 +6,6 @@ var user_word;
 
 var last = 0;
 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
 // setTimeout(show_page_animation(), 5);
 
 // function show_page_animation() {
